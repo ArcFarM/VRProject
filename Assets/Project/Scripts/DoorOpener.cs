3d1eef5dfa321cd6fb18464b94179783d88d8cfa
남기정 <namkijeong5955@gmail.com>
2024-05-08 10:44:21 +0900

    손님 움직이기(미완성)

--- Assets/Project/Scripts/DoorOpener.cs
+++ Assets/Project/Scripts/DoorOpener.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class DoorOpener : MonoBehaviour
 {
     public Vector3 pivot;
-    public float doorOpenAngle = -90.0f;
-    public float doorCloseAngle = 0.0f;
-    public float smooth = 2.0f;
-    public bool open = false;
+    public float doorOpenAngle = -90.0f;            //문 열릴 각도
+    public float doorCloseAngle = 0.0f;             //문 닫힐 각도
+    public float smooth = 2.0f;                     //열리는 속도
+    public bool open = false;                       //열릴지 말지 결정하는 flag변수
 
     private void Start()
     {
@@ -18,7 +18,7 @@ public class DoorOpener : MonoBehaviour
     IEnumerator Open()
     {
         open = true;
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(3);     //열린 후 3초 뒤에 닫히게 함
         open = false;
     }
 
@@ -26,13 +26,13 @@ public class DoorOpener : MonoBehaviour
     {
         if (open)
         {
-            Quaternion targetRotationOpen = Quaternion.Euler(0, doorOpenAngle, 0);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotationOpen, smooth * Time.deltaTime);
+            Quaternion targetRotationOpen = Quaternion.Euler(0, doorOpenAngle, 0);      //문이 열릴 때의 각도 설정
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotationOpen, smooth * Time.deltaTime);       //자연스럽게 문 열림
         }
         else
         {
-            Quaternion targetRotationClose = Quaternion.Euler(0, doorCloseAngle, 0);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotationClose, smooth * Time.deltaTime);
+            Quaternion targetRotationClose = Quaternion.Euler(0, doorCloseAngle, 0);    //문이 닫힐 때의 각도 설정
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotationClose, smooth * Time.deltaTime);      //자연스럽게 문 닫힘
         }
     }
 }
---
+++ Assets/Project/Scripts/Move_Guest.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Theme.Primitives;
+
+public class Move_Guest : MonoBehaviour
+{
+    public Transform StartingPos;       //손님의 출발 지점
+    public Vector3 DoorstepPos;          //문 앞의 도착 지점
+    public float MoveSpeed = 3.0f;      //이동 속도
+    public float RotationSpeed = 45.0f;  //문 앞 도착 후 회전 속도
+    public float RotationAngle = -90.0f;  //문 앞 도착 후 회전 각도
+    private Vector3 CounterPos = new Vector3(4.25f, 1.9f, 32.274f);       //카운터 위치
+
+    public bool isMoving = true;       //손님이 이동 중인지 확인하는 flag 변수
+    void Start()
+    {
+        StartingPos = transform;
+        DoorstepPos = transform.position;
+        DoorstepPos.x = 5.6f;           //도착 지점 좌표 설정
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, DoorstepPos, MoveSpeed * Time.deltaTime);       //문앞까지 손님 이동
+            if (transform.position == DoorstepPos)       // 문 앞 도착 시 돌고 카운터로 가는 코루틴 시작
+            {
+                isMoving = false;
+                StartCoroutine(Rotate());
+            }
+            if(transform.position == CounterPos)        // 카운터 앞에 도착 시 코루틴 중지
+            {
+                isMoving = false;
+                StopCoroutine(Rotate());
+            }
+        }
+    }
+
+    IEnumerator Rotate()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0, RotationAngle, 0);      //도착 후 회전 각도 설정 
+        while (transform.rotation != targetRotation)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);       //회전
+            yield return null;      // 다 돌 때까지 대기
+        }
+
+        yield return new WaitForSeconds(0.25f);     //0.25초 후 손님 이동
+        DoorstepPos = CounterPos;        //카운터로 이동
+        isMoving = true;
+        Debug.Log(DoorstepPos);
+        Debug.Log(isMoving);
+    }
+}
