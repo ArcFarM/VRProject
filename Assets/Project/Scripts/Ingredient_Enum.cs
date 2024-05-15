9ed493a306472ab267b36e4bb170e74501e6e3de
ArcFarM <56999471+ArcFarM@users.noreply.github.com>
2024-05-15 13:51:19 +0900

    열거형 순서 변경
    보기 편한거라 구성 변화는 X

--- Assets/Project/Scripts/Ingredient_Enum.cs
+++ Assets/Project/Scripts/Ingredient_Enum.cs
@@ -5,10 +5,10 @@ namespace Ing_Enum {
     public enum Ing_List {
         //밑에 오는 빵, 기본 색
         Bun_L, Bun_L_Grilled, Bun_L_Grilled_Ketchup,
-        //위에 오는 빵, 기본 색
-        Bun_U, Bun_U_Grilled, Bun_U_Sesame,
         //밑에 오는 빵, 검은 색
         Bun_Black_L, Bun_Black_L_Mustard,
+        //위에 오는 빵, 기본 색
+        Bun_U, Bun_U_Grilled, Bun_U_Sesame,
         //위에 오는 빵, 검은 색
         Bun_Black_U, Bun_Black_U_Grilled, Bun_Black_U_Sesame,
         //일반 재료
