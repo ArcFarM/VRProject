765a767b1d617a8863514ae85ee5c9840bf31c81
남기정 <namkijeong5955@gmail.com>
2024-05-23 10:29:44 +0900

    응애

--- Assets/Resources/Project/Scripts/Pouring_Sauce.cs
+++ Assets/Resources/Project/Scripts/Pouring_Sauce.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using Ing_Enum;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class Pouring_Sauce : MonoBehaviour
 {
     public GameObject Poured_Bread;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name.Contains("Bun"))
