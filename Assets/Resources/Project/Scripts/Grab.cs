bdc4ba33939472881799cdf5e3c184918d64f557
남기정 <namkijeong5955@gmail.com>
2024-06-03 19:34:44 +0900

    시작 햄버거 메뉴 구현 완료
    텍스트 남긴함

--- Assets/Resources/Project/Scripts/Grab.cs
+++ Assets/Resources/Project/Scripts/Grab.cs
@@ -6,9 +6,16 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Grab : MonoBehaviour
 {
     public XRGrabInteractable grabInteractable;
+    public GameObject otherBurger;                  //다른 버거
+    public bool isStart = false;                    //true면 Start버거 false면 Exit버거
     void Start()
     {
-        if(grabInteractable != null)
+        //게임을 정지시켜 버리면 Grab도 못해서 아주 느리게 시간이 흐르도록 설정
+        if (isStart == true)
+        {
+            Time.timeScale = 0.01f;
+        }
+        if (grabInteractable != null)
         {
             grabInteractable.selectEntered.AddListener(OnGrabbed);
         }
@@ -20,7 +27,7 @@ public class Grab : MonoBehaviour
 
     void OnDestory()
     {
-        if(grabInteractable != null)
+        if (grabInteractable != null)
         {
             grabInteractable.selectEntered.RemoveListener(OnGrabbed);
         }
@@ -28,12 +35,36 @@ public class Grab : MonoBehaviour
 
     void OnGrabbed(SelectEnterEventArgs args)
     {
-        StartGame();
+        if (isStart == true)
+        {
+            StartGame();
+        }
+        else
+        {
+            ExitGame();
+        }
         gameObject.SetActive(false);
+        otherBurger.SetActive(false);
+    }
+
+    void ExitGame()
+    {
+        Debug.Log("Game Exited!");
+        //게임 종료
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        //#if UNITY_EDITOR 전처리기를 사용하여 Unity 에디터에서 실행 중인 경우
+        //UnityEditor.EditorApplication.isPlaying = false;를 통해 에디터 플레이 모드를 종료합니다.
+        //Application.Quit();를 통해 실제 빌드된 애플리케이션을 종료합니다.
     }
 
     void StartGame()
     {
         Debug.Log("Game Started!");
+        //게임 시작
+        Time.timeScale = 1f;
     }
 }
